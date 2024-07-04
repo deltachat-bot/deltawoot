05a55fcf148b083e5c09932b8fcd78c426c97b6e
missytake <missytake@systemli.org>
2024-07-04 12:36:50 +0200

    send attachments from woot to delta

--- src/deltawoot/send.py
+++ src/deltawoot/send.py
@@ -1,5 +1,7 @@
 import deltachat_rpc_client
 from flask import Flask, request
+import requests
+from pprint import pprint
 
 
 def create_app(ac: deltachat_rpc_client.Account):
@@ -8,11 +10,32 @@ def create_app(ac: deltachat_rpc_client.Account):
     @app.post("/")
     def pass_woot_to_delta():
         email = request.json['conversation']['meta']['sender']['email']
-        if request.json['conversation']['messages'][0]['sender'].get('email') != email:
-            text = request.json['conversation']['messages'][0]['processed_message_content']
+        message = request.json['conversation']['messages'][0]
+        pprint(message)
+        if message['sender'].get('email') != email:
             chat = ac.create_contact(email).create_chat()
+            text = message['processed_message_content']
             chat.send_text(text)
+            for attachment in message.get('attachments', []):
+                url = attachment['data_url'].replace('http://', 'https://').replace(':3000/rails', '/rails')
+                filename = download_file(url)
+                chat.send_file(filename)
             return "successfully delivered"
         return "message was from outside contact"
 
     return app
+
+
+def download_file(url: str) -> str:
+    """Download a file and return the file name.
+
+    :param url: the URL of the file, usually https://chatwoot.testrun.org/rails/active_storage/blobs/...
+    :return: the file name of the file on the system.
+    """
+    r = requests.get(url, stream=True)
+    file_name = url.split("/")[-1]
+    with open(file_name, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+    return file_name
