473c812b57d4fcb4f772869c76f1258e0a8b5f86
missytake <missytake@systemli.org>
2026-04-01 15:57:57 +0200

    fix: ignore requests without inbox

--- src/deltawoot/send.py
+++ src/deltawoot/send.py
@@ -21,7 +21,9 @@ def create_app(ac: deltachat_rpc_client.Account):
 
     @app.post("/")
     def pass_woot_to_delta():
-        if not request.json["inbox"]["id"] == ac.woot.inbox_id:
+        if not request.json.get("inbox"):
+            return "ignore request without Inbox"
+        if not request.json.get("inbox")["id"] == ac.woot.inbox_id:
             logging.info("Inbox %s ignoring msg from other Inbox %s" % (ac.woot.inbox_id, request.json["inbox"]["id"]))
             return "ignoring message from other Inbox"
         if not request.json.get("conversation"):
